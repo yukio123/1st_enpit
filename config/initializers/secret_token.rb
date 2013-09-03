b4c88108aa934c07b4048bce794a9ccd917c471b
Yukio Saitoh <yukio.saitoh@gmail.com>
2013-09-04 01:49:40 +0900

    1st hogehoge

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstEnpit::Application.config.secret_key_base = '51023d0768d4976078c5030f7f16888a9c3bafe0eb82ed5daf8a2764ad6a93af00f196d4fb23378d800dc3dc87e44a3ecd2fea3663529bccf42bb5d76a381cd3'
